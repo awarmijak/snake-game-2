f9709594299c3366a9a67483e819589b5972e1d6
Warmijak <Aguszka9*>
2021-03-21 22:17:47 +0100

    Added methods to GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GamePanel extends JPanel implements ActionListener {
 
@@ -8,8 +11,41 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver (Graphics g) {
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
 }
